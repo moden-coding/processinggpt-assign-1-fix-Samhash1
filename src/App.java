a94e30f1cfff8d985ad31ce15b9c66815a7ab2f1
Sam <28shashkes@gannacademy.org>
2024-09-24 09:45:29 -0400

    Done

--- src/App.java
+++ src/App.java
@@ -4,4 +4,24 @@ public class App extends PApplet {
     public static void main(String[] args)  {
         PApplet.main("App");
     }
+    public void settings() {
+        size(800, 600); // Set window size to 800x600
+    }
+     public void setup(){
+        strokeWeight( 3);
+        stroke(0, 0, 200);
+        fill(255, 0, 0);
+        triangle(600, 100, 700, 200, 500, 200);
+        
+       
+        strokeWeight( 5);
+        stroke(0, 200, 0 );
+        fill(0, 180, 0);
+        rect(50, 50, 200, 100);
+       
+        strokeWeight( 50);
+        stroke(0, 100, 200);
+        line(50, 50, 600, 100);
+     }
+
 }
